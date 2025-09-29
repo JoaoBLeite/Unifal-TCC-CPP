40b26ecac87122aa5b964fdfadf68f31b04f2ac2
JoaoBLeite <johny.student.dev@gmail.com>
2025-09-29 20:29:54 -0300

    Adding dijkstra with list into factory

--- src/main/java/br/com/unifal/tcc/solvers/factories/PathAlgorithmFactory.java
+++ src/main/java/br/com/unifal/tcc/solvers/factories/PathAlgorithmFactory.java
@@ -1,14 +1,21 @@
 package br.com.unifal.tcc.solvers.factories;
 
+import br.com.unifal.tcc.algorithms.DijkstraListAlgorithm;
 import br.com.unifal.tcc.algorithms.DijkstraPqAlgorithm;
 import br.com.unifal.tcc.algorithms.SSSPAlgorithm;
 import br.com.unifal.tcc.algorithms.interfaces.ShortestPathAlgorithm;
 
 public enum PathAlgorithmFactory {
-  DIJKSTRA {
+  DIJKSTRA_PQ {
     @Override
     public ShortestPathAlgorithm createAlgorithm() {
       return new DijkstraPqAlgorithm();
+    }
+  },
+  DIJKSTRA_LIST {
+    @Override
+    public ShortestPathAlgorithm createAlgorithm() {
+      return new DijkstraListAlgorithm();
     }
   },
   SSSP {
